a2374b087b5f42003ec9deffa843b7bed72cb8b5
Isaac Lambat <isaac.lambat@couchbase.com>
2022-04-06 13:12:21 +0100

    Fixed file path being omitted

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const lineReader = __webpack_require__(631);
 const fs = __webpack_require__(747);
 
 try {
-    const regex = /(\w+_test.go:\d+:)/g; // Extracts file name and line where test failures occurred
+    const regex = /[\w\d]+_test.go:\d+:/iu; // Extracts file name and line where test failures occurred
 
 	const testResultsPath = core.getInput('test-results');
 	const customPackageName = core.getInput('package-name');
@@ -61,9 +61,9 @@ try {
 				const result = regex.exec(value);
 				if (result != null) {
 					const parts = result[0].split(":");
-					const file = parts[0]
+                    const file = key.split("/").slice(0, -1).join("/") + "/" + parts[0];
 					const lineNumber = parts[1];
-					core.info(`::error file=${file},line=${lineNumber}::${value}`)
+					core.info(`::error file=${file},line=${lineNumber}::${value}`);
 				}
 			}
 		}
